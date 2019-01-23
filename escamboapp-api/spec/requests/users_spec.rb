db5b752d22fba1cab81b88e74c74ebc30fce336e
Gustavo Olegário <gustavo-olegario@hotmail.com>
2019-01-23 21:43:05 -0200

    Writing test for users controller

---
+++ escamboapp-api/spec/requests/users_spec.rb
@@ -0,0 +1,113 @@
+require 'rails_helper'
+
+RSpec.describe 'User API', type: :request do
+  # Initialize data
+  let!(:user) { create(:user, 1) }
+  let!(:posts) { create_list(:post, 10) }
+  let(:post_id) { posts.first.id }
+
+  # Test suite for GET /users
+  describe 'GET /users' do
+    # make HTTP get request before each example
+    before { get '/users' }
+
+    it 'returns user' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(1)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # Test suite for GET /users/:id
+  describe 'GET /users/:id' do
+    before { get "/users/#{user_id}" }
+
+    context 'when the record exists' do
+      it 'returns the user' do
+        expect(json).not_to be_empty
+        expect(json['id']).to eq(user_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:user_id) { 100 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find User/)
+      end
+    end
+
+    # Test suite for POST /users
+    describe 'POST /users' do
+      let(:valid_attributes) {
+        {
+          name: 'Gustavo Olegario',
+          cpf: '09991356940',
+          email: 'gustavo-olegario@hotmail.com'
+        }
+      }
+
+      context 'when the request is valid' do
+        before { post '/users', params: valid_attributes }
+
+        it 'creates a user' do
+          expect(json['name']).to eq('Gustavo Olegario')
+        end
+
+        it 'returns status code 201' do
+          expect(resposne).to have_http_status(201)
+        end
+      end
+
+      context 'when the request is invalid' do
+        before { post '/users', params: { name: 'Gustavo' } }
+
+        it 'returns status code 422' do
+          expect(response).to have_http_status(422)
+        end
+
+        it 'returns a validation failure message' do
+          expect(response.body)
+            .to match(/Validation failed: CPF can't be blank/)
+        end
+      end
+    end
+
+    # Test suite for PUT /users/:id
+    describe 'PUT /users/:id' do
+      let(:valid_attributes) { { name: 'Gustavo' } }
+
+      context 'when the record exists' do
+        before { put "/users/#{user_id}", params: valid_attributes }
+
+        it 'updates the record' do
+          expect(response.body).to be_empty
+        end
+
+        it 'returns status code 204' do
+          expect(response).to have_http_status(204)
+        end
+      end
+    end
+
+    # Test suite for DELETE /users/:id
+    describe 'DELETE /users/:id' do
+      before { delete "/users/#{user_id}" }
+
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+    end
+  end
+end
